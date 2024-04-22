03be29b395606332654c9b90a2c7d9b66362b745
Eduardo Exequiel Lucero <eduardoexequiel@gmail.com>
2024-04-22 20:24:26 +0100

    subida

--- Array/src/Main.java
+++ Array/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -6,10 +7,21 @@ public class Main {
         Scanner t = new Scanner(System.in);
         ArrayList<Integer> numero=new ArrayList<>();
         int n=0;
+        int contador=0;
         for(int i=0;i<3;i++) {
             System.out.println("ingrese un numero:");
             n= t.nextInt();
             numero.add(n);
+            contador=contador+1;
+        }
+        for(int i=0;i<3;i++) {
+            System.out.println("los numeros ingresados son:" +numero.get(i));
+
+        }
+        Collections.sort(numero);
+
+        for(int i=0;i<3;i++) {
+            numero.remove(i);
         }
 
     }
